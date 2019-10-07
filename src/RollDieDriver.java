94556a16442ab83bd4d4d2a56e93e3bd1c7093b0
thefluffyoshi <katielynntrinh@gmail.com>
2019-10-07 13:50:49 -0700

    Initial commit test

---
+++ src/RollDieDriver.java
@@ -0,0 +1,12 @@
+public class RollDieDriver
+{
+    // 1. Create dies
+
+    // 2. Print it out
+
+    // 3. Test getters
+
+    // 4. Test setters
+
+    // 5. Check methods and properties if applicable
+}
---
+++ src/rollDie.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class rollDie
+{
+    // 1) Instance variables
+    Scanner keyboard = new Scanner(System.in);
+    boolean wantRoll;
+    int side;
+    int howManyRoll;
+
+    // 2) Constructors
+    public rollDie()
+    {
+        side = 0;
+        howManyRoll = 0;
+        wantRoll = false;
+    } //end default constructor
+
+
+
+
+} //end class rollDie
