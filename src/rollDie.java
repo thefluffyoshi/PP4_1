e7b01b5fa95019a71b51c4f8c46cd84783d4afab
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2019-10-10 13:46:55 -0700

    in progress

--- src/rollDie.java
+++ src/rollDie.java
@@ -4,20 +4,23 @@ public class rollDie
     boolean wantRoll;
     int side;
     int howManyRoll;
+    int numRoll;
 
     // 2) Constructors
     public rollDie()
     {
         side = 2;
         howManyRoll = 1;
+        numRoll = 1;
         wantRoll = false;
     } //end default constructor
 
-    public rollDie(boolean newWantRoll, int newSide, int newHowManyRoll)
+    public rollDie(boolean newWantRoll, int newSide, int newHowManyRoll, int newNumRoll)
     {
         wantRoll = newWantRoll;
         howManyRoll = newHowManyRoll;
-        side = newSide;
+        side = checkSide(newSide);
+        numRoll = newNumRoll;
     } //end multi constructor
 
     // 3) Getters
@@ -34,5 +37,37 @@ public class rollDie
     public int getHowManyRoll()
     {
         return howManyRoll;
-    }
+    }//end howManyRoll getter
+
+    // 4) Setters
+    public void setSide(int newSide)
+    {
+        side = newSide;
+    }//end side setter
+
+
+    // 5) toString
+    public String toString()
+    {
+        String output = "";
+        output += "If rolls are wanted: " + wantRoll;
+        output += "\nNumber of rolls: " + howManyRoll;
+        output += "\nSide: " + side;
+        return output;
+    }//end toString
+
+    // 6) Brain Method
+    private int checkSide(int newSide)
+    {
+        int tempSide = newSide;
+        if (tempSide > 2)
+        {
+            tempSide = newSide;
+        }//end if
+        else if (tempSide < 2)
+        {
+            tempSide = 3;
+        }//end else if
+        return tempSide;
+    }//end validating that it's actually rollable checkSide
 } //end class rollDie
