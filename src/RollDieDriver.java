afe7bfb2aaabb6c59629d802eb923d6125d7bbb1
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2019-10-10 13:56:42 -0700

    in progress

--- src/RollDieDriver.java
+++ src/RollDieDriver.java
@@ -9,9 +9,13 @@ public class RollDieDriver
         System.out.println(die1);
 
         // 3. Test getters
-        System.out.println(die1.getSide());
+        //System.out.println(die1.getSide());
 
         // 4. Test setters
+        System.out.println("\n" + die1);
+        die1.setWantRoll(true);
+        die1.setSide(5);
+        die1.setHowManyRoll(4);
 
         // 5. Check methods and properties if applicable
     } //end main
